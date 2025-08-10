5dd4f614c6f63910d031df5ad50f2ff9f44ba1df
Parth <parth.manocha2901@gmail.com>
2025-08-10 09:28:54 +0530

    updated cors issue!!

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,10 @@ const questionRoutes = require('./routes/questions');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  credentials: true,
+}));
 app.use(express.json());
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
